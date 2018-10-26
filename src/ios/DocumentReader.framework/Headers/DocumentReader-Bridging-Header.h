a326693248021d05969b9a82f3bbd629f0e659ce
Dmitry Smolyakov <dm.smolyakov@gmail.com>
2018-10-26 11:31:06 +0300

    API updated

--- src/ios/DocumentReader.framework/Headers/DocumentReader-Bridging-Header.h
+++ src/ios/DocumentReader.framework/Headers/DocumentReader-Bridging-Header.h
@@ -1,11 +1,3 @@
-//
-//  DocumentReader-Bridging-Header.h
-//  DocumentReader
-//
-//  Created by Игорь Клещёв on 04.06.17.
-//  Copyright © 2017 Regula Forensics. All rights reserved.
-//
-
 #ifndef DocumentReader_Bridging_Header_h
 #define DocumentReader_Bridging_Header_h
 
